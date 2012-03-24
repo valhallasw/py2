a90daa8e681a17c9f21a7a70c598926e92574525
Merlijn van Deen <valhallasw+dorthonion@gmail.com>
2012-03-24 01:03:20 +0100

    Changed server.py so that it dies when sys.stdin is closed. This means it closes automatically once the client process exits

--- server.py
+++ server.py
@@ -10,7 +10,6 @@ from six import print_
 
 import logging
 if "-silent" not in sys.argv:
-    print sys.argv
     logging.basicConfig(level=logging.DEBUG)
 else:
     logging.basicConfig(level=logging.ERROR)
@@ -20,32 +19,17 @@ fc = reference.FauxConnection()
 class FCManager(Manager):
     pass
 
-#for fun in ['get_global', 'call', 'get_attribute', 'destroy']:
-#    def wrapfungen(fun):
-#        def wrapfun(*args, **kwargs):
-#            res = getattr(fc, fun)(*args, **kwargs)
-#            print fun, args, kwargs, "->", res
-#            return res
-#        return wrapfun
-#    
-#    FCManager.register(fun,wrapfungen(fun))
-
 FCManager.register('get_manager', lambda: fc)
 
-m = FCManager(address=('127.0.0.1', 13337), serializer='bPickle', authkey='a')
-s = m.get_server()
+m = FCManager(address=('127.0.0.1', 13338), serializer='bPickle', authkey='a')
+m.start()
 
-print_(json.dumps([s.address, s.authkey.decode('latin-1')]))
+print_(json.dumps([m._address, m._authkey.decode('latin-1')]))
 sys.stdout.flush()
-print dir(s)
-import threading
 
-#class JetzerThread(threading.Thread):
-#def run(self):
-#        s.serve_forever()
-#
-#JetzerThread().start()
-
-import os; print os.getpid()
-
-s.serve_forever()
+x = True
+while x:
+    try:
+        x = sys.stdin.read()
+    except KeyboardInterrupt:
+        print "(python2 server) ignoring KeyboardInterrupt. Use ^D to quit when running directly."
